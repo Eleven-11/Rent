7d6f43660b55ca690951c705243f56881c725749
xxxs9 <751723378@qq.com>
2019-08-12 22:57:19 +0800

    帖子类别添加页码

--- back/src/main/java/com/heeexy/example/dao/PostTypeDao.java
+++ back/src/main/java/com/heeexy/example/dao/PostTypeDao.java
@@ -19,11 +19,18 @@ public interface PostTypeDao {
     List<JSONObject> getPostTypeList(JSONObject jsonObject);
 
     /**
-     * 添加帖子类型信息
+     * 后台：获取帖子类型列表
      * @param jsonObject
      * @return
      */
-    int insertPostType(JSONObject jsonObject);
+    List<JSONObject> getBackPostTypeList(JSONObject jsonObject);
+
+    /**
+     * 后台：添加帖子类型信息
+     * @param jsonObject
+     * @return
+     */
+    int countBackPostType(JSONObject jsonObject);
 
     /**
      * 根据帖子类型id修改相应信息
@@ -43,6 +50,13 @@ public interface PostTypeDao {
      * @return
      */
     int countPostType(JSONObject jsonObject);
+
+    /**
+     * 添加帖子类型信息
+     * @param jsonObject
+     * @return
+     */
+    int insertPostType(JSONObject jsonObject);
 
     /**
      * @description 帖子八大类型排序
