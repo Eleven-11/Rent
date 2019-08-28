d465d8336cb28790736e32f5050197eda76996d3
eleven-11 <1228657497@qq.com>
2019-08-28 12:42:32 +0800

    用户浏览接口更新

--- back/src/main/java/com/heeexy/example/util/WxConfig.java
+++ back/src/main/java/com/heeexy/example/util/WxConfig.java
@@ -9,9 +9,9 @@ package com.heeexy.example.util;
  **/
 public class WxConfig {
     //微信APPID
-    public static final String  WX_APP_ID="wx21ada25dcb46cf67";
+    public static final String  WX_APP_ID="wx496d13b03d0397f3";
 
     //微信appSecret
-    public static final String WX_APP_SECRET="8cf1327adc98f5decb23c4b0147fcb90";
+    public static final String WX_APP_SECRET="b7033127ad43e754ad24c99aed7a2609";
 
 }
